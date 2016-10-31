4d6db49ceabbbf777d6d565940f091d26c242866
elhendricks <elhendricks@gmail.com>
2016-10-30 21:47:03 -0700

    edited to pass linter

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('./lib/server');
 const port = 3000;
 
-server.listen(3000, (err, req) => {
+server.listen(3000, (err) => {
     if (err) console.log('ERR: ', err);
     else console.log('Server is listening on port: ', port);
 });
